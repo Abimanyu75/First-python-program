574fb4024908c7a6ffe4dd7dfe908bdcb1ef1f9e
Abimanyu75 <abimanyu7595@gmail.com>
2026-08-15 13:37:17 +0530

    Add files via upload
    My first python code

---
+++ Python Assignment 1.py
@@ -0,0 +1,20 @@
+'''1.Print student details like name,id,marks for 5 subject and average'''
+
+a=input("Enter student name")
+b= int(input("Enter id number"))
+c=int(input("Enter marks for English"))
+d=int(input("Enter marks for Tamil"))
+e=int(input("Enter marks for Maths"))
+f=int(input("Enter marks for Science"))
+g=int(input("Enter marks for Social"))
+
+print("Average =", a, b, (c+d+e+f+g)/5)
+
+'''2.Print addition of three number[user_input]'''
+
+a=int(input("Enter the first number="))
+b=int(input("Enter the second number="))
+c=int(input("Enter the third number="))
+
+print("Total",a+b+c)
+
